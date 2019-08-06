0897b3546f61694700791ca8041cda80b076b7a8
gilrann <gilrann@users.github.com>
2019-08-06 21:29:38 +0700

    itsallaboutthis

---
+++ js/gilrann.js
@@ -0,0 +1,21 @@
+$('.slider').glide({
+
+  autoplay: false,
+  arrows: false,
+  nav: true
+
+});
+
+
+			// var glide = $('.slider').glide().data('api_glide');
+      //
+			// 	$(window).on('keyup', function (key) {
+			// 		if (key.keyCode === 13) {
+			// 			glide.jump(3, console.log('Wooo!'));
+			// 		};
+			// 	});
+      //
+			// 	$('.slider-arrow').on('click', function() {
+			// 		console.log(glide.current());
+			// 	});
+      //
